e8475fbeb89b73f2b547736f8cd75370f0fce965
Wael Mohammed <wmamohammed1@sheffield.ac.uk>
2022-03-06 02:28:56 +0000

    Added the Sum of Squared Errors

--- R/goodness_of_fit.R
+++ R/goodness_of_fit.R
@@ -8,8 +8,8 @@
 #' contains the values (column name 'value') and standard errors (column
 #' name 'sd') of the corresponding target.
 #'
-#' @return A table with proposed parameter sets and their corresponding log
-#' likelihood values.
+#' @return A table with proposed parameter sets and their corresponding
+#' summed overall likelihood values sorted in descending order.
 #' @export
 #'
 #' @examples
@@ -98,17 +98,41 @@ log_likelihood <- function(.func, .args = list(NULL), .samples,
 #' A function to estimate weighted sum of squared errors (WSSE)
 #' goodness-of-fit
 #'
-#' @param .func
-#' @param .args
-#' @param .samples
-#' @param .l_targets
+#' @param .func A function defining the model to be calibrated
+#' @param .args A list of arguments to be passed to .func
+#' @param .samples A table with sampled parameter values
+#' @param .l_targets A list containing a vector of targets' names, and a
+#' table for each target that contains the values (column name 'value') and
+#' standard errors (column name 'sd') of the corresponding target.
+#' @param .maximise Logical for whether the output of the function is used
+#' in a maximising optimisation function. Default is \code{TRUE}.
+#' @param .weighted Logical for whether the SSR was to be weighted, default
+#' is \code{TRUE}. The weight used by function is \code{1/(sd^2)}.
 #'
-#' @return
+#' @return A table with proposed parameter sets and their corresponding
+#' summed overall weighted sum of square values sorted in descending order.
 #' @export
 #'
 #' @examples
-wSSE_GOF <- function(.func, .args = list(NULL), .samples,
-                     .l_targets) {
+#' data("CRS_targets")
+#' Surv <- CRS_targets$Surv
+#' v_targets_names <- c("Surv")
+#' l_targets <- list('v_targets_names' = v_targets_names, 'Surv' = Surv)
+#' v_params_names <- c("p_Mets", "p_DieMets")
+#' v_params_dists <- c("unif", "unif")
+#' args <- list(list(min = 0.04, max = 0.16),
+#'              list(min = 0.04, max = 0.12))
+#' samples <- sample_prior_LHS(
+#'            .l_params = list(v_params_names = v_params_names,
+#'                            v_params_dists = v_params_dists,
+#'                            args = args),
+#'            .n_samples = 10000)
+#'
+#' GOF_wsse <- wSSE_GOF(.func = CRS_markov, .samples = samples,
+#'                      .l_targets = l_targets)
+#'
+wSSE_GOF <- function(.func, .args = list(NULL), .samples, .weighted = TRUE,
+                     .l_targets, .maximise = TRUE) {
   # Run the model using each set of sampled parameters:
   model_results <- pmap(
     .l = .samples,
@@ -116,32 +140,37 @@ wSSE_GOF <- function(.func, .args = list(NULL), .samples,
       dots <- list(...)
       exec(.func, dots, !!!.args)
     })
-  # Define inputs list for the log likelihood function:
-  l_llk <- list(.l_targets[['v_targets_names']],
-                paste0('d', .l_targets[['v_targets_dists']]),
-                .l_targets[['v_targets_dists']],
-                model_results)
-  # Estimate the log likelihood for each model output:
-  log_likelihood <- pmap_dfc(
-    .l = l_llk,
-    .f = function(.name, .func, .dist, .mod_res) {
-      if(.dist != 'lnorm') {
-        exec(.func,
-             .l_targets[[.name]]$value,
-             .mod_res[[.name]],
-             .l_targets[[.name]]$se,
-             log = TRUE)
-      } else {
-        exec(.func,
-             .l_targets[[.name]]$value,
-             log(.mod_res[[.name]]) - (1/2) * .l_targets[[.name]]$se^2,
-             .l_targets[[.name]]$se,
-             log = TRUE)
-      }
+  # Define inputs list for the weighted sum of squared errors function:
+  l_wsse <- list(.l_targets[['v_targets_names']])
+  # Estimate the weighted sum of squared errors for each model output:
+  wsse <- pmap(
+    .l = l_wsse,
+    .f = function(.name) {
+      map_dbl(
+        .x = model_results,
+        .f = function(.mod_res) {
+          if(.weighted) {
+            weight <- 1/(.l_targets[[.name]]$se^2)
+            -sum(weight *
+                   ((.l_targets[[.name]]$value - .mod_res[[.name]])^2))
+          } else {
+            -sum((.l_targets[[.name]]$value - .mod_res[[.name]])^2)
+          }
+        })
     })
+  # Overall log likelihood (over all targets):
+  overall_weighted_sse <- pmap_dbl(
+    .l = wsse,
+    .f = function(...) {
+      dots = list(...) # grab all targets' wsse for proposed parameter sets
+      reduce(.x = dots, .f = `+`, .init = 1) # sum them together
+    })
+  # Amend output if optimisation function was a minimiser (flip signs):
+  if(!.maximise)
+    overall_weighted_sse <- -overall_weighted_sse
   # Prepare the output table:
   output <- .samples %>%
-    mutate('Overall_fit' = colSums(log_likelihood)) %>%
+    mutate('Overall_fit' = overall_weighted_sse) %>%
     arrange(desc(Overall_fit))
 
   return(output)
